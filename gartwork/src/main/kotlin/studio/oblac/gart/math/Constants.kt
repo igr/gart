4a7b0b775403816dddfdf8b2436d084d84d0935a
Igor Spasić <igor.spasic@gmail.com>
2023-08-28 10:05:47 +0200

    Add Distance and constants.

--- gartwork/src/main/kotlin/studio/oblac/gart/math/Constants.kt
+++ gartwork/src/main/kotlin/studio/oblac/gart/math/Constants.kt
@@ -1,8 +1,12 @@
 package studio.oblac.gart.math
 
+// todo remove object
 object Constants {
-    val goldenRatio = 1.61803398875
-    val wideScreenRatio = 16.0 / 9
-    val ultraWideScreenRatio = 21.0 / 9
-    val cinemaRatio = 2.4
+    const val goldenRatio = 1.61803398875
+    const val wideScreenRatio = 16.0 / 9
+    const val ultraWideScreenRatio = 21.0 / 9
+    const val cinemaRatio = 2.4
 }
+
+const val PIf = Math.PI.toFloat()
+const val DOUBLE_PIf = 2 * Math.PI.toFloat()
