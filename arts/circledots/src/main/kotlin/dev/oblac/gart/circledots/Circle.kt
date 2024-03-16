cf081ad8dd3b8fd1798e599d5e651120db351451
Igor Spasić <igor.spasic@gmail.com>
2024-03-16 22:40:34 +0100

    Update Circledots

--- arts/circledots/src/main/kotlin/dev/oblac/gart/circledots/Circle.kt
+++ arts/circledots/src/main/kotlin/dev/oblac/gart/circledots/Circle.kt
@@ -3,21 +3,22 @@ package dev.oblac.gart.circledots
 import dev.oblac.gart.gfx.fillOfBlack
 import dev.oblac.gart.gfx.strokeOfBlack
 import dev.oblac.gart.math.subDeg
+import dev.oblac.gart.skia.Canvas
 
 data class Circle(private val ctx: Context, val x: Float, val y: Float, val r: Float, var deg: Float, var speed: Float = 3f) {
 	private var rOffset = 4
 
-	fun draw(drawCircle: Boolean) {
+    fun draw(canvas: Canvas, drawCircle: Boolean) {
 		val rr = r + ctx.msin[deg] * rOffset
 
 		if (drawCircle) {
-			ctx.g.canvas.drawCircle(x, y, rr, blackStroke)
+            canvas.drawCircle(x, y, rr, blackStroke)
 		}
 
 		val x2 = x + ctx.mcos[deg] * rr
 		val y2 = y - ctx.msin[deg] * rr
 
-		ctx.g.canvas.drawCircle(x2, y2, 4f, blackFill)
+        canvas.drawCircle(x2, y2, 4f, blackFill)
 		deg = deg.subDeg(speed)
 	}
 
