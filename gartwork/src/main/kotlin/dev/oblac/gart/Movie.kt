55832b173a76d868779d7992ee7ad4b51e97babe
Igor Spasić <igor.spasic@gmail.com>
2024-03-16 10:09:59 +0100

    Update examples

--- gartwork/src/main/kotlin/dev/oblac/gart/Movie.kt
+++ gartwork/src/main/kotlin/dev/oblac/gart/Movie.kt
@@ -6,19 +6,31 @@ import dev.oblac.gart.skia.Image
 /**
  * Movie is just a simple buffer of snapshot images.
  */
-class Movie(val d: Dimension, private val name: String) {
+class Movie(val d: Dimension, private val name: String, startRecording: Boolean = true) {
     private val allFrames = mutableListOf<Image>()
     private val gartvas = Gartvas(d)
     private val bitmap = gartvas.createBitmap()
 
+    private var recording = startRecording
+
     fun addFrame(gartvas: Gartvas) {
+        if (!recording) return
         allFrames.add(gartvas.snapshot())
     }
 
     fun addFrame(canvas: Canvas) {
+        if (!recording) return
         canvas.readPixels(bitmap, 0, 0)
         gartvas.writeBitmap(bitmap)
         addFrame(gartvas)
+    }
+
+    fun startRecording() {
+        recording = true
+    }
+
+    fun stopRecording() {
+        recording = false
     }
 
     fun forEachFrame(consumer: (Int, Image) -> Unit) = allFrames.forEachIndexed(consumer)
@@ -28,7 +40,8 @@ class Movie(val d: Dimension, private val name: String) {
     /**
      * Decorates window with movie recording.
      */
-    fun record(window: Window): Window {
+    fun record(window: Window, recording: Boolean = this.recording): Window {
+        this.recording = recording
         return object : Window(d) {
             override fun show(drawFrame: DrawFrame) {
                 super.show { c, d, f ->
