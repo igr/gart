25c2a3a6907d80182617e5eb1814eda3235ad8b2
Igor Spasić <igor.spasic@gmail.com>
2024-10-23 22:11:00 +0200

    Add angles

--- gart/src/main/kotlin/dev/oblac/gart/angles/Angles.kt
+++ gart/src/main/kotlin/dev/oblac/gart/angles/Angles.kt
@@ -1,28 +1,47 @@
 package dev.oblac.gart.angles
 
+import dev.oblac.gart.math.DOUBLE_PIf
 import dev.oblac.gart.math.PIf
 
 @JvmInline
 value class Radians(internal val value: Float) {
     fun degrees() = Degrees(value * 180 / PIf)
 
-    operator fun plus(delta: Radians) =
-        Radians(value + delta.value)
+    operator fun plus(r: Radians) =
+        Radians(value + r.value)
 
-    operator fun times(value: Number): Radians =
-        Radians(value.toFloat() * this.value)
+    operator fun plus(n: Number) =
+        Radians(value + n.toFloat())
 
-    operator fun div(value: Number): Radians =
-        Radians(this.value / value.toFloat())
+    operator fun minus(r: Radians) =
+        Radians(value - r.value)
+
+    operator fun minus(n: Number) =
+        Radians(value - n.toFloat())
+
+    operator fun times(n: Number): Radians =
+        Radians(n.toFloat() * this.value)
+
+    operator fun div(n: Number): Radians =
+        Radians(this.value / n.toFloat())
 
     fun toFloat() = value
+
+    fun normalize(): Radians {
+        var result = value
+        while (result < 0) {
+            result += DOUBLE_PIf
+        }
+        while (result > DOUBLE_PIf) {
+            result -= DOUBLE_PIf
+        }
+        return Radians(result)
+    }
 
     companion object {
         val ZERO: Radians = Radians(0f)
     }
 }
-
-//operator fun Number.times(radians: Radians) = Radians(this.toFloat() * radians.value)
 
 fun cos(r: Radians): Double {
     return kotlin.math.cos(r.value.toDouble())
@@ -32,16 +51,47 @@ fun sin(r: Radians): Double {
     return kotlin.math.sin(r.value.toDouble())
 }
 
+/**
+ * Calculates the middle angle between two angles.
+ * The result is always in the range of: -PI..PI.
+ * This means that the result is always the shortest angle between the two angles.
+ */
+fun middleAngle(a: Radians, b: Radians): Radians {
+    return when (val diff = (b - a).value) {
+        in -PIf..PIf -> (a + diff / 2).normalize()
+        in PIf..DOUBLE_PIf -> (a + diff / 2 - PIf).normalize()
+        in -DOUBLE_PIf..-PIf -> (a + diff / 2 + PIf).normalize()
+        in DOUBLE_PIf..2 * DOUBLE_PIf -> (a + (diff - DOUBLE_PIf) / 2).normalize()
+        in -2 * DOUBLE_PIf..-DOUBLE_PIf -> (a + (diff + DOUBLE_PIf) / 2).normalize()
+        else -> throw IllegalStateException("Unexpected angle difference: $diff")
+    }
+}
+
+
+// --- DEGREES ---
+
+
 @JvmInline
 value class Degrees(internal val value: Float) {
     fun radians() = Radians(value * PIf / 180)
 
-    operator fun plus(delta: Degrees): Degrees {
-        return Degrees(value + delta.value)
+    operator fun plus(delta: Degrees): Degrees =
+        Degrees(value + delta.value)
+
+    operator fun minus(delta: Degrees): Degrees =
+        Degrees(value - delta.value)
+
+    fun normalize(): Degrees {
+        var result = value
+        while (result < 0) {
+            result += 360
+        }
+        while (result > 360) {
+            result -= 360
+        }
+        return Degrees(result)
     }
 }
-
-//operator fun Number.times(degrees: Degrees) = Degrees(this.toFloat() * degrees.value)
 
 fun cos(d: Degrees): Double =
     kotlin.math.cos(d.radians().value.toDouble())
