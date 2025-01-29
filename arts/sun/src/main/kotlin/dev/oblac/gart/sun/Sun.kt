9c04c0b61cf1fbaf1c1479c27f72cfb860c001f9
Igor Spasić <igor.spasic@gmail.com>
2025-01-29 18:11:01 +0100

    Sun.

--- arts/sun/src/main/kotlin/dev/oblac/gart/sun/Sun.kt
+++ arts/sun/src/main/kotlin/dev/oblac/gart/sun/Sun.kt
@@ -3,30 +3,42 @@ package dev.oblac.gart.sun
 import dev.oblac.gart.Gart
 import dev.oblac.gart.color.Colors
 import dev.oblac.gart.color.Palettes
-import dev.oblac.gart.fluid.lbh.Simulation
+import dev.oblac.gart.fluid.lbh.LatticeBoltzmannSimpleFluid
+import kotlin.math.abs
+import kotlin.math.sin
 
 val p = Palettes.cool43.expand(200)
 
 fun main() {
-    val gart = Gart.of("sun", 1025, 1025, 10)
+    val gart = Gart.of("sun", 1024, 1024, 10)
     val g = gart.gartvas()
     val w = gart.window()
     val m = gart.movieGif()
 
-    val lbf = Simulation(1025, 1025)
+    val lbf = LatticeBoltzmannSimpleFluid(1024, 1024)
+    lbf.init { l, x, y ->
+        l.density = sin(y / 512f) + sin(x / 512f) - 1f
+        l.velocityX = 0.3f
+        l.velocityY = 0.0f
+    }
 
     val bitmap = gart.gartmap(g)
     val movieEnds = 100L
-//    m.record(w).show { c, _, f ->
-    w.show { c, _, f ->
+    m.record(w).show { c, _, f ->
+        //w.show { c, _, f ->
         c.clear(Colors.white)
-        lbf.iterate()
-        lbf.renderByDensity(c)
-//        lbf.renderByVelocity(c)
+        lbf.simulate()
+        lbf.lattices { l, x, y ->
+            val a = abs(l.density % 1f)
+            val clr = p.safe(a * p.size)
+            bitmap[x, y] = clr
+        }
 
-//        f.onFrame(movieEnds) {
-//            m.stopRecording()
-//            gart.saveMovie(m)
-//        }
+        c.drawImage(bitmap.image(), 0f, 0f)
+
+        f.onFrame(movieEnds) {
+            m.stopRecording()
+            gart.saveMovie(m)
+        }
     }
 }
