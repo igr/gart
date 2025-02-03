f3e69dc95592c13ce08d9c2aeba024659c300eed
Igor Spasić <igor.spasic@gmail.com>
2025-02-03 22:29:00 +0100

    Remove particles

--- gart/src/main/kotlin/dev/oblac/gart/particle/particles.kt
+++ gart/src/main/kotlin/dev/oblac/gart/particle/particles.kt
@@ -5,27 +5,6 @@ import org.jetbrains.skia.Point
 data class Particle(
     val point: Point,
     val vx: Double,
-    val vy: Double,
-    val yD: Int
+    val vy: Double
 )
 
-data class Row(
-    val yD: Int,
-    val particles: List<Particle>
-)
-
-class ParticleSystem(
-    val r: Float,
-    val d: Float = r * 2
-) {
-    private var particles: MutableList<Particle> = mutableListOf()
-
-    fun x() {
-        val sorted = particles
-            .map { it.copy(yD = (it.point.y / d).toInt()) }
-            .sortedBy { it.yD }
-            .groupByTo(mutableMapOf()) { it.yD }
-            .entries
-            .map { Row(it.key, it.value.sortedBy { p -> p.point.x }) }
-    }
-}
