5cc74a4e7f97994e4648e51d5f28c18950b3ef9c
Igor Spasić <igor.spasic@gmail.com>
2025-02-07 23:41:10 +0100

    NS1

---
+++ arts/sun/src/main/kotlin/dev/oblac/gart/sun/ns1/SunNs1.kt
@@ -0,0 +1,79 @@
+package dev.oblac.gart.sun.ns1
+
+import dev.oblac.gart.Gart
+import dev.oblac.gart.color.Colors
+import dev.oblac.gart.color.Palettes
+import dev.oblac.gart.fluid.navstr.NavierStokesSolver
+
+val p = Palettes.cool21.reversed().expand(200)
+
+fun main() {
+    val gart = Gart.of("sunNS1", 800, 800, 10)
+    val d = gart.d
+    val g = gart.gartvas()
+    val w = gart.window()
+
+    val fluidWidth = d.w
+    val fluidHeight = d.h
+
+    val ns = NavierStokesSolver(
+        fluidWidth, fluidHeight,
+        dt = 1/10f,
+        fadeSpeed = 0f,
+        viscocity = 0.00001f
+    )
+
+    addForce(ns, 0.4f, 0.4f, 1f, 0f)
+    addForce(ns, 0.6f, 0.6f, -1f, 1f)
+
+    val bitmap = gart.gartmap(g)
+    w.show { c, _, f ->
+        c.clear(Colors.white)
+
+        for (x in 0..<ns.nx) {
+            for (y in 0..<ns.ny) {
+//                bitmap[x, y] = p.bound(s.u(x, y) * 1e8)
+                bitmap[x, y] = p.bound(ns.v(x, y) * 1e8)
+            }
+        }
+
+        c.drawImage(bitmap.image(), 0f, 0f)
+        ns.update()
+
+        f.onFrame(230) {
+            gart.saveImage(c)
+        }
+    }
+}
+
+
+fun addForce(s: NavierStokesSolver, xx: Float, yy: Float, dx: Float, dy: Float) {
+    var x = xx
+    var y = yy
+    val aspectRatio = 1f
+    val speed = dx * dx + dy * dy * aspectRatio // balance the x and y
+    if (speed > 0) {
+        if (x < 0) x = 0f
+        else if (x > 1) x = 1f
+        if (y < 0) y = 0f
+        else if (y > 1) y = 1f
+
+        val velocityMult = 150.0f
+
+        val index = s.indexForNormalizedPosition(x, y)
+
+        // colorMode(HSB, 360, 1, 1);
+        // float hue = ((x + y) * 180 + frameCount) % 360;
+        // drawColor = color(hue, 1, 1);
+        // colorMode(RGB, 1);
+        //
+        // fluidSolver.rOld[index] += red(drawColor) * colorMult;
+        // fluidSolver.gOld[index] += green(drawColor) * colorMult;
+        // fluidSolver.bOld[index] += blue(drawColor) * colorMult;
+
+        // particleSystem.addParticles(x * width, y * height, 10);
+        s.uOld[index] += dx * velocityMult
+        s.vOld[index] += dy * velocityMult
+    }
+}
+
---
+++ gart/src/main/kotlin/dev/oblac/gart/fluid/navstr/NavierStokesSolver.kt
@@ -0,0 +1,380 @@
+package dev.oblac.gart.fluid.navstr
+
+import kotlin.math.floor
+import kotlin.math.min
+
+/**
+ * Solving real-time fluid dynamics simulations based on Navier-Stokes equations
+ * and code from Jos Stam's paper "Real-Time Fluid Dynamics for Games".
+ * http://www.dgp.toronto.edu/people/stam/reality/Research/pdf/GDC03.pdf
+ */
+@Suppress("t")
+class NavierStokesSolver(
+    val nx: Int,
+    val ny: Int,
+    val dt: Float = 1f,     // time step
+    val viscocity: Float = 0f, // viscosity
+    val fadeSpeed: Float = 0f,  // how quickly the fluid dye dissipates and fades out
+    val solverIterations: Int = 10 // number of iterations for solver (higher is slower but more accurate)
+) {
+    val width = nx + 2
+    val height = ny + 2
+
+    private val invWidth = 1.0f / nx
+    private val invHeight = 1.0f / ny
+
+    val numCells = (nx + 2) * (ny + 2)  // Total number of cells
+
+    private val invNx = 1.0f / nx
+    private val invNy = 1.0f / ny
+    private val invNumCells = 1.0f / numCells
+
+    var r = FloatArray(numCells)
+    var u = FloatArray(numCells)
+    var v = FloatArray(numCells)
+
+    var rOld = FloatArray(numCells)
+    var uOld = FloatArray(numCells)
+    var vOld = FloatArray(numCells)
+
+    private var avgDensity = 0f // Average density of fluid
+    private var avgUniformity = 0f // average uniformity (distribution of densities and dye)
+    private var avgSpeed = 0f    // average speed of fluid (how uniform the colors is)
+
+    /**
+     * Randomize dye (useful for debugging).
+     */
+    fun randomizeColor() {
+        for (i in 0..<width) {
+            for (j in 0..<height) {
+                val index = fluidIX(i, j)
+                rOld[index] = Math.random().toFloat()
+                r[index] = rOld[index]
+            }
+        }
+    }
+
+    /**
+     * Returns fluid cell index for (i,j) cell coordinates.
+     * @return cell index (to be used in r, u, v arrays)
+     */
+    fun indexForCellPosition(i: Int, j: Int): Int {
+        val ii = when {
+            i < 1 -> 1
+            i > nx -> nx
+            else -> i
+        }
+        val jj = when {
+            j < 1 -> 1
+            j > ny -> ny
+            else -> j
+        }
+
+        return fluidIX(ii, jj)
+    }
+
+    /**
+     * Returns fluid cell index for normalized (x, y) coordinates.
+     * @param x 0...1 normalized x position
+     * @param y 0...1 normalized y position
+     * @return cell index (to be used in r, u, v arrays)
+     */
+    fun indexForNormalizedPosition(x: Float, y: Float): Int {
+        return indexForCellPosition(
+            floor((x * (nx + 2))).toInt(),
+            floor((y * (ny + 2))).toInt()
+        )
+    }
+
+    fun addForceAtPos(x: Float, y: Float, vx: Float, vy: Float) {
+        val i = (x * nx + 1)
+        val j = (y * ny + 1)
+        if (i < 0 || i > x + 1 || j < 0 || j > y + 1) return;
+        addForceAtCell(i.toInt(), j.toInt(), vx, vy);
+    }
+
+    fun addForceAtCell(i: Int, j: Int, vx: Float, vy: Float) {
+        val index = fluidIX(i, j)
+        uOld[index] += vx * nx
+        vOld[index] += vy * ny
+    }
+    
+    /**
+     * Updates the calculation. Must be called once every frame to move the
+     * solver one step forward.
+     */
+    fun update() {
+        addSourceUV()
+
+        swapU()
+        swapV()
+
+        diffuseUV(0, viscocity)
+
+        project(u, v, uOld, vOld)
+
+        swapU()
+        swapV()
+
+        advect(1, u, uOld, uOld, vOld)
+        advect(2, v, vOld, uOld, vOld)
+
+        project(u, v, uOld, vOld)
+
+
+        addSource(r, rOld)
+        swapR()
+
+        diffuse(0, r, rOld, 0f)
+        swapR()
+
+        advect(0, r, rOld, u, v)
+        fadeR()
+    }
+
+    private fun fadeR() {
+        // I want the fluid to gradually fade out so the screen doesn't fill.
+        // the amount it fades out depends on how full it is, and how uniform
+        // (i.e. boring) the fluid is...
+        // float holdAmount = 1 - _avgDensity * _avgDensity * _fadeSpeed;
+        // this is how fast the density will decay depending on how full the
+        // screen currently is
+        val holdAmount = 1 - fadeSpeed
+
+        avgDensity = 0f
+        avgSpeed = 0f
+
+        var totalDeviations = 0f
+        var currentDeviation: Float
+
+        // float uniformityMult = uniformity * 0.05f;
+        avgSpeed = 0f
+        for (i in 0..<numCells) {
+            // clear old values
+            vOld[i] = 0f
+            uOld[i] = vOld[i]
+            rOld[i] = 0f
+
+            // gOld[i] = bOld[i] = 0;
+
+            // calc avg speed
+            avgSpeed += u[i] * u[i] + v[i] * v[i]
+
+            // calc avg density
+            r[i] = min(1.0, r[i].toDouble()).toFloat()
+            // float density = Math.max(r[i], Math.max(g[i], b[i]));
+            val density = r[i]
+            avgDensity += density // add it up
+
+            // calc deviation (for uniformity)
+            currentDeviation = density - avgDensity
+            totalDeviations += currentDeviation * currentDeviation
+
+            // fade out old
+            r[i] *= holdAmount
+        }
+        avgDensity *= invNumCells
+
+        // _avgSpeed *= _invNumCells;
+
+        avgUniformity = 1.0f / (1 + totalDeviations * invNumCells)
+        // 0: very wide distribution
+        // 1: very uniform
+    }
+
+    private fun addSourceUV() {
+        for (i in 0..<numCells) {
+            u[i] += dt * uOld[i]
+            v[i] += dt * vOld[i]
+        }
+    }
+
+    private fun addSource(x: FloatArray, x0: FloatArray) {
+        for (i in 0..<numCells) {
+            x[i] += dt * x0[i]
+        }
+    }
+
+    private fun advect(b: Int, d: FloatArray, d0: FloatArray, du: FloatArray, dv: FloatArray) {
+        val dt0 = dt * nx
+
+        for (i in 1..nx) {
+            for (j in 1..ny) {
+                var x = i - dt0 * du[fluidIX(i, j)]
+                var y = j - dt0 * dv[fluidIX(i, j)]
+
+                if (x > nx + 0.5) x = nx + 0.5f
+                if (x < 0.5) x = 0.5f
+
+                val i0 = x.toInt()
+                val i1 = i0 + 1
+
+                if (y > ny + 0.5) y = ny + 0.5f
+                if (y < 0.5) y = 0.5f
+
+                val j0 = y.toInt()
+                val j1 = j0 + 1
+
+                val s1 = x - i0
+                val s0 = 1 - s1
+                val t1 = y - j0
+                val t0 = 1 - t1
+
+                d[fluidIX(i, j)] = (s0 * (t0 * d0[fluidIX(i0, j0)] + t1 * d0[fluidIX(i0, j1)])
+                    + s1 * (t0 * d0[fluidIX(i1, j0)] + t1 * d0[fluidIX(i1, j1)]))
+            }
+        }
+        setBoundary(b, d)
+    }
+
+    private fun diffuse(b: Int, c: FloatArray, c0: FloatArray, diff: Float) {
+        val a = dt * diff * nx * ny
+        linearSolver(b, c, c0, a, 1.0f + 4 * a)
+    }
+
+    private fun diffuseUV(b: Int, diff: Float) {
+        val a = dt * diff * nx * ny
+        linearSolverUV(b, a, 1.0f + 4 * a)
+    }
+
+    private fun project(x: FloatArray, y: FloatArray, p: FloatArray, div: FloatArray) {
+        for (i in 1..nx) {
+            for (j in 1..ny) {
+                div[fluidIX(i, j)] = (x[fluidIX(i + 1, j)] - x[fluidIX(i - 1, j)]
+                    + y[fluidIX(i, j + 1)] - y[fluidIX(i, j - 1)]) * -0.5f / nx
+                p[fluidIX(i, j)] = 0f
+            }
+        }
+
+        setBoundary(0, div)
+        setBoundary(0, p)
+
+        linearSolver(0, p, div, 1f, 4f)
+
+        for (i in 1..nx) {
+            for (j in 1..ny) {
+                x[fluidIX(i, j)] -= 0.5f * nx * (p[fluidIX(i + 1, j)] - p[fluidIX(i - 1, j)])
+                y[fluidIX(i, j)] -= 0.5f * nx * (p[fluidIX(i, j + 1)] - p[fluidIX(i, j - 1)])
+            }
+        }
+
+        setBoundary(1, x)
+        setBoundary(2, y)
+    }
+
+    private fun linearSolver(b: Int, x: FloatArray, x0: FloatArray, a: Float, c: Float) {
+        for (k in 0..<solverIterations) {
+            for (i in 1..nx) {
+                for (j in 1..ny) {
+                    x[fluidIX(
+                        i,
+                        j
+                    )] = (a * (x[fluidIX(i - 1, j)] + x[fluidIX(i + 1, j)]
+                        + x[fluidIX(i, j - 1)] + x[fluidIX(i, j + 1)])
+                        + x0[fluidIX(i, j)]) / c
+                }
+            }
+            setBoundary(b, x)
+        }
+    }
+
+    private fun linearSolverUV(bound: Int, a: Float, c: Float) {
+        for (k in 0..<solverIterations) { // MEMO
+            for (i in 1..nx) {
+                for (j in 1..ny) {
+                    val index5 = fluidIX(i, j)
+                    val index1 = index5 - 1 // FLUID_IX(i-1, j);
+                    val index2 = index5 + 1 // FLUID_IX(i+1, j);
+                    val index3 = index5 - (nx + 2) // FLUID_IX(i, j-1);
+                    val index4 = index5 + (nx + 2) // FLUID_IX(i, j+1);
+
+                    u[index5] = ((a * (u[index1] + u[index2] + u[index3] + u[index4]) + uOld[index5]) / c)
+                    v[index5] = ((a * (v[index1] + v[index2] + v[index3] + v[index4]) + vOld[index5]) / c)
+                    // x[FLUID_IX(i, j)] = (a * ( x[FLUID_IX(i-1, j)] +
+                    // x[FLUID_IX(i+1, j)] + x[FLUID_IX(i, j-1)] + x[FLUID_IX(i,
+                    // j+1)]) + x0[FLUID_IX(i, j)]) / c;
+                }
+            }
+            setBoundaryRGB(bound)
+        }
+    }
+
+    private fun setBoundary(b: Int, x: FloatArray) {
+        // return;
+        for (i in 1..nx) {
+            if (i <= ny) {
+                x[fluidIX(0, i)] = if (b == 1) -x[fluidIX(1, i)] else x[fluidIX(1, i)]
+                x[fluidIX(nx + 1, i)] = if (b == 1) -x[fluidIX(nx, i)] else x[fluidIX(nx, i)]
+            }
+
+            x[fluidIX(i, 0)] = if (b == 2) -x[fluidIX(i, 1)] else x[fluidIX(i, 1)]
+            x[fluidIX(i, ny + 1)] = if (b == 2) -x[fluidIX(i, ny)] else x[fluidIX(i, ny)]
+        }
+
+        x[fluidIX(0, 0)] = 0.5f * (x[fluidIX(1, 0)] + x[fluidIX(0, 1)])
+        x[fluidIX(0, ny + 1)] = 0.5f * (x[fluidIX(1, ny + 1)] + x[fluidIX(0, ny)])
+        x[fluidIX(nx + 1, 0)] = 0.5f * (x[fluidIX(nx, 0)] + x[fluidIX(nx + 1, 1)])
+        x[fluidIX(nx + 1, ny + 1)] = (0.5f
+            * (x[fluidIX(nx, ny + 1)] + x[fluidIX(nx + 1, ny)]))
+    }
+
+    private fun setBoundaryRGB(bound: Int) {
+        var index1: Int
+        var index2: Int
+        for (i in 1..nx) {
+            if (i <= ny) {
+                index1 = fluidIX(0, i)
+                index2 = fluidIX(1, i)
+                r[index1] = if (bound == 1) -r[index2] else r[index2]
+
+                index1 = fluidIX(nx + 1, i)
+                index2 = fluidIX(nx, i)
+                r[index1] = if (bound == 1) -r[index2] else r[index2]
+            }
+
+            index1 = fluidIX(i, 0)
+            index2 = fluidIX(i, 1)
+            r[index1] = if (bound == 2) -r[index2] else r[index2]
+
+            index1 = fluidIX(i, ny + 1)
+            index2 = fluidIX(i, ny)
+            r[index1] = if (bound == 2) -r[index2] else r[index2]
+        }
+
+        // x[FLUID_IX( 0, 0)] = 0.5f * (x[FLUID_IX(1, 0 )] + x[FLUID_IX( 0,
+        // 1)]);
+        // x[FLUID_IX( 0, _NY+1)] = 0.5f * (x[FLUID_IX(1, _NY+1)] + x[FLUID_IX(
+        // 0, _NY)]);
+        // x[FLUID_IX(_NX+1, 0)] = 0.5f * (x[FLUID_IX(_NX, 0 )] +
+        // x[FLUID_IX(_NX+1, 1)]);
+        // x[FLUID_IX(_NX+1, _NY+1)] = 0.5f * (x[FLUID_IX(_NX, _NY+1)] +
+        // x[FLUID_IX(_NX+1, _NY)]);
+    }
+
+    private fun swapU() {
+        val tmp = u
+        u = uOld
+        uOld = tmp
+    }
+
+    private fun swapV() {
+        val tmp = v
+        v = vOld
+        vOld = tmp
+    }
+
+    private fun swapR() {
+        val tmp = r
+        r = rOld
+        rOld = tmp
+    }
+
+    private fun fluidIX(i: Int, j: Int) = ((i) + (nx + 2) * (j))
+
+    // protected void ADD_SOURCE_UV() { addSource(u, uOld); addSource(v, vOld);}
+    // protected void DIFFUSE_UV() { diffuse(0, u, uOld, visc); diffuse(0, v, vOld, visc); }
+
+    fun u(i: Int, j: Int) = u[fluidIX(i, j)]
+    fun v(i: Int, j: Int) = v[fluidIX(i, j)]
+
+}
