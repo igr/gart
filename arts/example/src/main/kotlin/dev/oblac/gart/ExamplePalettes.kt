f9b5e4e4f0bebf921abc6a6d6dc0360b3b59188e
Igor Spasić <igor.spasic@gmail.com>
2025-04-27 18:41:31 +0200

    COLORS!

---
+++ arts/example/src/main/kotlin/dev/oblac/gart/ExamplePalettes.kt
@@ -0,0 +1,89 @@
+package dev.oblac.gart
+
+import dev.oblac.gart.color.Palette
+import dev.oblac.gart.color.Palettes
+import dev.oblac.gart.gfx.fillOf
+import dev.oblac.gart.gfx.ofXYWH
+import org.jetbrains.skia.Canvas
+import org.jetbrains.skia.Rect
+
+private val coolPalette: (Int) -> Palette = { Palettes.coolPalette(it) }
+private val mixPalette: (Int) -> Palette = { Palettes.mixPalette(it) }
+private fun paletteOf(type: Int) = when (type) {
+    1 -> coolPalette
+    2 -> mixPalette
+    else -> throw IllegalArgumentException("Unknown palette type: $type")
+}
+
+fun main() {
+    val gart = Gart.of("example palettes", 1024, 1024)
+    println(gart)
+
+    val w = gart.window()
+    var index = 1
+    var horizontal = false
+    var reverse = false
+    var type = 1
+
+    w.show { c, d, f ->
+        val p = if (reverse) {
+            paletteOf(type)(index).reversed()
+        } else {
+            paletteOf(type)(index)
+        }
+        if (horizontal) {
+            drawPaletteL2R(c, d, p)
+        } else {
+            drawPaletteT2B(c, d, p)
+        }
+    }.onKey {
+        when (it) {
+            Key.KEY_W -> {
+                index--
+            }
+
+            Key.KEY_S -> {
+                index++
+            }
+
+            Key.KEY_Q -> {
+                horizontal = !horizontal
+            }
+
+            Key.KEY_E -> {
+                reverse = !reverse
+            }
+
+            Key.KEY_SPACE -> {
+                type = 1 + (type % 2)
+                index = 1
+            }
+
+            else -> {}
+        }
+        println("index: $index")
+    }
+}
+
+// draw rectangles of the palette, left to right
+fun drawPaletteL2R(c: Canvas, d: Dimension, p: Palette) {
+    val w = d.wf / p.size
+    val h = d.hf
+    for (i in p.indices) {
+        val color = p[i]
+        c.drawRect(
+            Rect.ofXYWH(i * w, 0f, w, h), fillOf(color)
+        )
+    }
+}
+
+fun drawPaletteT2B(c: Canvas, d: Dimension, p: Palette) {
+    val w = d.wf
+    val h = d.hf / p.size
+    for (i in p.indices) {
+        val color = p[i]
+        c.drawRect(
+            Rect.ofXYWH(0f, i * h, w, h), fillOf(color)
+        )
+    }
+}
