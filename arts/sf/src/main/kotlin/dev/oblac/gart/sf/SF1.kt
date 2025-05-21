23ee6db2b90e8efc1d1a01b84ffda50a29bc84a3
Igor Spasić <igor.spasic@gmail.com>
2025-05-21 23:05:40 +0200

    SF1

---
+++ arts/sf/src/main/kotlin/dev/oblac/gart/sf/SF1.kt
@@ -0,0 +1,83 @@
+package dev.oblac.gart.sf
+
+import dev.oblac.gart.Dimension
+import dev.oblac.gart.Gart
+import dev.oblac.gart.color.RetroColors
+import dev.oblac.gart.gfx.*
+import org.jetbrains.skia.Canvas
+import org.jetbrains.skia.Path
+import org.jetbrains.skia.Point
+import kotlin.math.cos
+import kotlin.math.sin
+
+fun main() {
+    val gart = Gart.of("sf1", 1024, 1024)
+    println(gart)
+
+    val d = gart.d
+    val w = gart.window()
+
+    val g = gart.gartvas()
+    val c = g.canvas
+    draw(c, d)
+    gart.saveImage(g)
+    w.showImage(g)
+}
+
+private val colorBack = RetroColors.black01
+private val colorLine = RetroColors.white01
+private val colorSun = RetroColors.red01
+
+//private val colorSun = 0xFFcb4d23
+private fun draw(c: Canvas, d: Dimension) {
+    c.clear(colorBack)
+
+    // diagonal lines starting from right corner of the image
+    val paths = mutableListOf<Line>()
+    var gap = 110f
+    val steps = 24
+    for (i in 0..steps) {
+        val pointBottom = Point(d.wf - i * gap, d.hf)
+        val pointRight = Point(d.wf, d.hf - i * gap * 0.5f) // angle
+
+        val line = Line(pointRight, pointBottom)
+        val longerLine = Line(line.pointFromStartLen(-50f), line.pointFromEndLen(-50f))
+
+        paths.add(longerLine)
+
+        gap -= 1.5f + sin(i * 0.1f)
+    }
+
+    val revs = paths.reversed()
+    revs.forEachIndexed { index, line ->
+
+        if (index < revs.size - 1) {
+            val strokeWidth = 20.1f - cos(index * 0.1f) * 20f
+            // drawing the next line
+            val nextLine = revs[index + 1]
+
+            // draw path first
+            val path = Path()
+            path.moveTo(nextLine.a)
+            path.lineTo(nextLine.b)
+            path.lineTo(d.wf, d.hf)
+            path.closePath()
+            c.drawPath(path, fillOf(colorBack))
+
+            // draw the line
+            c.drawLine(nextLine, strokeOf(colorLine, strokeWidth))
+        }
+        if (index == steps - 7) {
+            c.drawCircle(0f, d.hf + 100f, 700f, fillOf(colorSun))
+        }
+    }
+
+    val moonRadius = 40f
+    val moonDelta = 10f
+    val moonX = d.w3 * 2
+    val moonY = d.h3 / 2
+    c.drawCircle(moonX, moonY, moonRadius, fillOf(colorLine))
+    c.drawCircle(moonX - moonDelta, moonY + moonDelta, moonRadius, fillOf(colorBack))
+
+    c.drawRoundBorder(d, 10f, 44f, colorLine)
+}
---
+++ gart/src/main/kotlin/dev/oblac/gart/color/RetroColors.kt
@@ -0,0 +1,7 @@
+package dev.oblac.gart.color
+
+object RetroColors {
+    val black01 = 0xFF33332D.toIntColor()
+    val white01 = 0xFFCAC4A2.toIntColor()
+    val red01 = 0xFFCA4D23.toIntColor()
+}
