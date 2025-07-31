a3f4c0b933c7e8eac347505c98d59a1562ebf192
Igor Spasic <igor.spasic@gmail.com>
2025-07-31 16:48:58 +0200

    hD

--- arts/harmongraph/src/main/kotlin/dev/oblac/gart/harmongraph/formulas/hD.kt
+++ arts/harmongraph/src/main/kotlin/dev/oblac/gart/harmongraph/formulas/hD.kt
@@ -2,9 +2,11 @@ package dev.oblac.gart.harmongraph.formulas
 
 import dev.oblac.gart.Dimension
 import dev.oblac.gart.Gart
+import dev.oblac.gart.angles.Degrees
 import dev.oblac.gart.color.RetroColors
 import dev.oblac.gart.gfx.Point
 import dev.oblac.gart.gfx.plus
+import dev.oblac.gart.math.Transform
 import org.jetbrains.skia.Canvas
 import org.jetbrains.skia.Point
 import kotlin.math.cos
@@ -32,10 +34,12 @@ private val colorBold = RetroColors.teal01
 private fun draw(c: Canvas, d: Dimension) {
     c.clear(colorBack)
     drawTriangles(c, d, colorBack, colorFront)
+    val r = Transform.rotate(d.cx, d.cy, Degrees.of(45)) + Transform.rotate(d.cx, d.cy, Degrees.of(45))
     repeat(600) { i ->
         val deltaPhase = i * 0.1f
         funD(deltaPhase, deltaPhase)
             .map { it + d.center }
+            .map { r(it) }
             .zipWithNext()
             .forEach(drawHarmonographLine(c, d, colorFront, colorBold, 180))
     }
---
+++ gart/src/main/kotlin/dev/oblac/gart/math/transform.kt
@@ -0,0 +1,35 @@
+package dev.oblac.gart.math
+
+import dev.oblac.gart.angles.Angle
+import dev.oblac.gart.angles.cos
+import dev.oblac.gart.angles.sin
+import dev.oblac.gart.gfx.pointOf
+import org.jetbrains.skia.Point
+
+fun interface Transform {
+    operator fun invoke(point: Point): Point
+    operator fun plus(transform: Transform) = Transform { point -> transform(this.invoke(point)) }
+
+    companion object {
+        fun rotate(point: Point, angle: Angle): Transform {
+            return RotationTransform(point.x, point.y, angle)
+        }
+
+        fun rotate(x: Number, y: Number, angle: Angle): Transform {
+            return RotationTransform(x.toFloat(), y.toFloat(), angle)
+        }
+    }
+}
+
+enum class RotationDirection {
+    CW,
+    CCW
+}
+
+class RotationTransform(private val x: Float, private val y: Float, private val angle: Angle) : Transform {
+    override operator fun invoke(point: Point): Point {
+        val cx = x + (point.x - x) * cos(angle) - (point.y - y) * sin(angle)
+        val cy = y + (point.x - x) * sin(angle) + (point.y - y) * cos(angle)
+        return pointOf(cx, cy)
+    }
+}
