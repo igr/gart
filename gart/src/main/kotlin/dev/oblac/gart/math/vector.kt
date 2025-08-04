2127f1f2bf3f39aeeb6b7f828535747d29fdd23f
Igor Spasic <igor.spasic@gmail.com>
2025-08-04 14:31:57 +0200

    Add vector math

--- gart/src/main/kotlin/dev/oblac/gart/math/vector.kt
+++ gart/src/main/kotlin/dev/oblac/gart/math/vector.kt
@@ -4,17 +4,19 @@ import dev.oblac.gart.angles.Radians
 import kotlin.math.atan2
 import kotlin.math.cos
 import kotlin.math.sin
+import kotlin.math.sqrt
 
 data class Vector2(val x: Float, val y: Float) {
     constructor(x: Number, y: Number) : this(x.toFloat(), y.toFloat())
 
     operator fun plus(other: Vector2) = Vector2(x + other.x, y + other.y)
     operator fun minus(other: Vector2) = Vector2(x - other.x, y - other.y)
-    operator fun times(scalar: Float) = Vector2(x * scalar, y * scalar)
+    operator fun times(scalar: Number) = Vector2(x * scalar.toFloat(), y * scalar.toFloat())
+    operator fun times(other: Vector2) = Vector2(x * other.x, y * other.y)
     operator fun div(scalar: Float) = Vector2(x / scalar, y / scalar)
     fun dot(other: Vector2) = x * other.x + y * other.y
     fun cross(other: Vector2) = x * other.y - y * other.x
-
+    fun length() = sqrt(x * x + y * y)
     val magnitude by lazy { fastSqrt(x * x + y * y) }
     fun normalize() = this / magnitude
     fun rotate(angle: Float) = Vector2(
@@ -31,3 +33,18 @@ data class Vector2(val x: Float, val y: Float) {
         val ZERO = Vector2(0f, 0f)
     }
 }
+
+data class Vector3(val x: Float, val y: Float, val z: Float) {
+    operator fun plus(other: Vector3) = Vector3(x + other.x, y + other.y, z + other.z)
+    operator fun minus(other: Vector3) = Vector3(x - other.x, y - other.y, z - other.z)
+    operator fun times(scalar: Number) = Vector3(x * scalar.toFloat(), y * scalar.toFloat(), z * scalar.toFloat())
+    operator fun times(other: Vector3) = Vector3(x * other.x, y * other.y, z * other.z)
+    fun length() = sqrt(x * x + y * y + z * z)
+    fun normalize() = this * (1.0f / length())
+    fun dot(other: Vector3) = x * other.x + y * other.y + z * other.z
+}
+
+data class Matrix2(val a: Float, val b: Float, val c: Float, val d: Float) {
+    operator fun times(v: Vector2) = Vector2(a * v.x + b * v.y, c * v.x + d * v.y)
+}
+
