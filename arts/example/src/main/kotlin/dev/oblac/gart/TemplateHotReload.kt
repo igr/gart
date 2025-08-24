fa698187cdaad4552512a65a9fff87f8d7627f26
Igor Spasic <igor.spasic@gmail.com>
2025-08-24 19:12:55 +0200

    Add TemplateHotReload functionality with drawing capabilities

---
+++ arts/example/src/main/kotlin/dev/oblac/gart/TemplateHotReload.kt
@@ -0,0 +1,38 @@
+package dev.oblac.gart
+
+import dev.oblac.gart.gfx.draw
+import org.jetbrains.skia.Canvas
+
+fun main() {
+    val gart = Gart.of("template2", 1024, 1024)
+    println(gart)
+
+    val w = gart.window()
+    val g = gart.gartvas()
+    val draw = MyDraw(g)
+
+    // save image
+    //g.draw(draw)
+    //gart.saveImage(g)
+
+    w.show(draw).hotReload(g)
+}
+
+/**
+ * Hot reload requires a real class to be created, not a lambda.
+ */
+private class MyDraw(val g: Gartvas) : Drawing(g) {
+    val b = Gartmap(g)
+    override fun draw(c: Canvas, d: Dimension, f: Frames) {
+        draw(c, d)
+        //b.updatePixelsFromCanvas()
+        // draw pixels
+        //b.drawToCanvas()
+        c.draw(g)
+    }
+}
+
+
+private fun draw(c: Canvas, d: Dimension) {
+
+}
