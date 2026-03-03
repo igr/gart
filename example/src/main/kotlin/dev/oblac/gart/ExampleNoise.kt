65f54c081a9f406c78513c327389b715a7bef4dd
Igor Spasic <igor.spasic@gmail.com>
2026-03-03 08:30:49 +0100

    Add Perlin and Simplex noise implementations with example usage

--- example/src/main/kotlin/dev/oblac/gart/ExampleNoise.kt
+++ example/src/main/kotlin/dev/oblac/gart/ExampleNoise.kt
@@ -5,6 +5,7 @@ import dev.oblac.gart.gfx.drawImage
 import dev.oblac.gart.gfx.drawPoint
 import dev.oblac.gart.gfx.random
 import dev.oblac.gart.gfx.strokeOf
+import dev.oblac.gart.math.f
 import dev.oblac.gart.noise.*
 import org.jetbrains.skia.Canvas
 import org.jetbrains.skia.Point
@@ -34,6 +35,8 @@ fun main() {
                 Key.KEY_4 -> perlin2(c, d)
                 Key.KEY_5 -> poissonDiskSampling(c, d)
                 Key.KEY_6 -> poissonDiskSampling2(c, d)
+                Key.KEY_7 -> simplex(c, d)
+                Key.KEY_8 -> openSimplex(c, d)
                 else -> {}
             }
         }
@@ -95,5 +98,44 @@ private fun poissonDiskSampling2(c: Canvas, d: Dimension) {
     c.clear(CssColors.white)
     poissonDiskSamplingNoise(d, 10.0).forEach {
         c.drawPoint(it, color)
+    }
+}
+
+fun simplex(c: Canvas, d: Dimension) {
+    c.clear(CssColors.white)
+    val simplex = SimplexNoise
+    repeat(POINTS) { i ->
+
+        // scale is important for visualization.
+        // If it's too small, you'll get a very noisy, almost random pattern.
+        // If it's too large, you'll get a very smooth, almost uniform pattern.
+        // You want to find a balance that shows the characteristic "cloud-like"
+        // structure of simplex noise.
+        val scale = 0.01
+
+        val x = simplex.noise(i * scale, 0.0)
+        val y = simplex.noise(i * scale, 1000.0)
+
+        val a = ((x + 1) / 2) * d.wf
+        val b = ((y + 1) / 2) * d.hf
+
+        c.drawPoint(a.f(), b.f(), color)
+    }
+}
+
+fun openSimplex(c: Canvas, d: Dimension) {
+    c.clear(CssColors.white)
+    val openSimplex = OpenSimplexNoise()
+    repeat(POINTS) { i ->
+        // This way x and y sample from different regions of the noise field and won't be correlated.
+        // Good for visualization, but not so good if you want to use the noise for something like terrain generation where you want x and y to be correlated.
+        val x = openSimplex.random2D(i.toDouble(), 0.0)
+        val y = openSimplex.random2D(i.toDouble(), 1000.0)
+
+        // since result is in range -1..1, we need to map it to 0..1 before multiplying by width and height
+        val a = ((x + 1) / 2) * d.wf
+        val b = ((y + 1) / 2) * d.hf
+
+        c.drawPoint(a.f(), b.f(), color)
     }
 }
