4381ccae11e6e892a221008c2414080cede58cba
Igor Spasic <igor.spasic@gmail.com>
2026-03-04 23:08:10 +0100

    Refactor Flow11 to streamline path drawing and enhance visual effects

--- arts/flowforce/src/main/kotlin/dev/oblac/gart/flowforce/flow11/Flow11.kt
+++ arts/flowforce/src/main/kotlin/dev/oblac/gart/flowforce/flow11/Flow11.kt
@@ -2,12 +2,15 @@ package dev.oblac.gart.flowforce.flow11
 
 import dev.oblac.gart.Gart
 import dev.oblac.gart.angle.Degrees
-import dev.oblac.gart.color.NipponColors.col016_KURENAI
 import dev.oblac.gart.color.NipponColors.col234_GOFUN
+import dev.oblac.gart.color.Palettes
+import dev.oblac.gart.color.RetroColors
+import dev.oblac.gart.color.alpha
 import dev.oblac.gart.flow.Flow2
 import dev.oblac.gart.flow.FlowField
-import dev.oblac.gart.gfx.points
-import dev.oblac.gart.gfx.strokeOf
+import dev.oblac.gart.gfx.*
+import dev.oblac.gart.math.map
+import dev.oblac.gart.math.rndb
 import dev.oblac.gart.noise.OpenSimplexNoise
 import dev.oblac.gart.streamlines.StreamlineTracer
 import dev.oblac.gart.util.middle
@@ -27,21 +30,35 @@ fun main() {
         Flow2(Degrees.of(n), StreamlineTracer.STEP_SIZE)
     }
 
-    val tracer = StreamlineTracer(d, flowField)
-    val paths = tracer.trace()
+    val paths = StreamlineTracer(d, flowField).trace()
 
-    // assign random widths, sort thickest-first
-    c.clear(col016_KURENAI)
+    //val p = Palettes.cool9
+    val p = Palettes.cool19
 
-    // build outlines
+    c.clear(RetroColors.black01)
+
     paths.forEach { path ->
         val point = path.points().middle()
         val width = cos(point.x * 0.01f) * 6f + 8f
-        val paint = strokeOf(col234_GOFUN, width).apply {
+        val color = p.safe(map(width, 0, 14f, 0, p.size - 1).toInt())
+        val paint = strokeOf(color, width).apply {
             this.strokeCap = PaintStrokeCap.ROUND
         }
-        c.drawPath(path, paint)
+        if (rndb(1, 10)) {
+            path.toPoints(8).forEach {
+                c.drawCircle(it, width * 0.5f, fillOf(color.alpha(100)))
+            }
+        } else {
+            c.drawPath(path, paint)
+        }
     }
+
+    // nacrtaj dve polukruga koja su pomerena
+    Point(d.w3x2, d.h3x2).let {
+        c.drawCircle(it, 120f, fillOf(col234_GOFUN))
+        c.drawCircle(it, 120f, strokeOf(RetroColors.black01, 20f))
+    }
+
 
     //gart.saveImage(g)
     val w = gart.window()
